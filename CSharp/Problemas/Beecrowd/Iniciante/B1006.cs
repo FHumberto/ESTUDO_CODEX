a09afa04aa41ff7c6cf45071e0926176bd8b395f
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-15 19:11:56 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1006 e B1007

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1006.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+using System.Globalization;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1006 : Problema
+{
+    public override void PMain(string problema)
+    {
+        ProcedureHelpers.Cabecalho("1006", "Média 2");
+
+        double notaA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double notaB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double notaC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double media = ((notaA * 2) + (notaB * 3) + (notaC * 5)) / 10;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1007.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1007 : Problema
+{
+    public override void PMain(string problema)
+    {
+        ProcedureHelpers.Cabecalho("1007", "Diferença");
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+
+        int diferenca = (a * b) - (c * d);
+
+        Console.WriteLine($"DIFERENCA = {diferenca}");
+    }
+}
