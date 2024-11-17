2705b99c0c3278658f6d023b4084fef4c20f9883
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-17 13:13:43 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1008 e B1009

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1008.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1008 : Problema
+{
+    public override void PMain(string problema)
+    {
+        ProcedureHelpers.Cabecalho("B1008", "Salário");
+
+        int funcionarioId = int.Parse(Console.ReadLine());
+        int horasTrabalhadas = int.Parse(Console.ReadLine());
+        float valorHora = float.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+        float salario = horasTrabalhadas * valorHora;
+
+        Console.WriteLine($"NUMBER = {funcionarioId}");
+        Console.WriteLine($"SALARY = U$ {salario.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1009.cs
@@ -0,0 +1,20 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1009 : Problema
+{
+    public override void PMain(string problema)
+    {
+        ProcedureHelpers.Cabecalho("B1009", "Salário com Bônus");
+
+        _ = Console.ReadLine();
+        double salarioFixoFuncionario = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+        double totalVendasFuncionario = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+        double total = salarioFixoFuncionario + (totalVendasFuncionario * 0.15);
+
+        Console.WriteLine($"TOTAL = R$ {total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
