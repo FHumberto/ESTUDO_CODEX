54dfa71571ce32533b24c0889b897bbc6b176161
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-18 08:04:07 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1010

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1010.cs
@@ -0,0 +1,28 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1010 : Problema
+{
+    public override void PMain(string problema)
+    {
+        ProcedureHelpers.Cabecalho("Cálculo Simples");
+
+        int[] pecas = new int[2];
+        double[] custo = new double[2];
+        double total = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            _ = int.Parse(linha[0]);
+            pecas[i] = int.Parse(linha[1]);
+            custo[i] = double.Parse(linha[2], System.Globalization.CultureInfo.InvariantCulture);
+
+            total += pecas[i] * custo[i];
+        }
+
+        Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
