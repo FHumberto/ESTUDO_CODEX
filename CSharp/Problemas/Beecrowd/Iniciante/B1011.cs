c4760a39aa4459d0de96ee6f47a06a6897dc9979
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-19 08:14:01 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1011

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1011.cs
@@ -0,0 +1,17 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1011 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Esfera");
+
+        const double PI = 3.14159;
+        double raio = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+        double volume = (4 / 3.0) * PI * Math.Pow(raio, 3);
+        Console.WriteLine($"VOLUME = {volume.ToString("F3", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
