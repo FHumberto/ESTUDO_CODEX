107f813305c010a5c468c36689f304307c04eb80
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-21 20:09:42 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1014

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1013.cs
@@ -0,0 +1,23 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1013 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("O Maior");
+
+        string[] linha = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(linha[0]);
+        int b = int.Parse(linha[1]);
+        int c = int.Parse(linha[2]);
+
+        int maiorAB = (a + b + Math.Abs(a - b)) / 2;
+        int maiorAC = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+        Console.WriteLine($"{maiorAC} eh o maior");
+    }
+}
