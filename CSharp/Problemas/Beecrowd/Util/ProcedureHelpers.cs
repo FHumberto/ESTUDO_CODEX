afac11df94ba8286bca23ac2965ca1e90d5f3a1c
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-20 23:28:21 -0300

    [CSharp/Beecrowd] Refact: usa ternário na operação e revisa os nullable do procedure helpers

--- CSharp/Problemas/Beecrowd/Util/ProcedureHelpers.cs
+++ CSharp/Problemas/Beecrowd/Util/ProcedureHelpers.cs
@@ -36,23 +36,18 @@ internal static class ProcedureHelpers
 
     public static void Mensagem(int operacao, string? problema = null)
     {
-        if (operacao == 0)
-        {
-            Console.WriteLine($"Deseja executar o problema B{problema} novamente? [S/N]");
-        }
-        else
-        {
-            Console.WriteLine("Deseja executar o todo o programa novamente? [S/N]");
-        }
+        Console.WriteLine(operacao == 0
+            ? $"Deseja executar o problema B{problema} novamente? [S/N]"
+            : "Deseja executar o todo o programa novamente? [S/N]");
     }
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
     public static void Cabecalho(string titulo)
     {
-        StackTrace? stackTrace = new();
+        StackTrace stackTrace = new();
         StackFrame? callingFrame = stackTrace.GetFrame(1);
         var callingMethod = callingFrame.GetMethod();
-        string? className = callingMethod.DeclaringType.Name;
+        string className = callingMethod.DeclaringType.Name;
         Console.WriteLine($"{className} - {titulo}\n");
     }
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
