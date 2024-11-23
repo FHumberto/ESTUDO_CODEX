0254f33bbfdd0851983927ca949f3ac7220fe656
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-23 10:19:08 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1014 e B1015

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1014.cs
@@ -0,0 +1,19 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1014 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Consumo");
+
+        int distanciaTotal = int.Parse(Console.ReadLine());
+        double distanciaPercorrida = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+        double consumo = distanciaTotal / distanciaPercorrida;
+
+        Console.WriteLine($"{consumo.ToString("F3", System.Globalization.CultureInfo.InvariantCulture)} km/l");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1015.cs
@@ -0,0 +1,26 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1015 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Distância Entre dois Pontos");
+
+        double[] x = new double[2];
+        double[] y = new double[2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            x[i] = float.Parse(linha[0], System.Globalization.CultureInfo.InvariantCulture);
+            y[i] = float.Parse(linha[1], System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        double distancia = Math.Sqrt(Math.Pow(x[1] - x[0], 2) + Math.Pow(y[1] - y[0], 2));
+
+        Console.WriteLine(distancia.ToString("F4", System.Globalization.CultureInfo.InvariantCulture));
+    }
+}
