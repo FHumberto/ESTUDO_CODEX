7dc913b8ab2e8ed650ea8a851f332158d73d3ee9
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-25 07:45:38 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1019 e B1020

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1019.cs
@@ -0,0 +1,16 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1019 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Conversão de Tempo");
+
+        int tempo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{tempo / 3600}:{(tempo % 3600) / 60}:{tempo % 60}");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1020.cs
@@ -0,0 +1,18 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+
+internal class B1020 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Idade em Dias");
+
+        int dias = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{dias / 365} ano(s)");
+        Console.WriteLine($"{(dias % 365) / 30} mes(es)");
+        Console.WriteLine($"{(dias % 365) % 30} dia(s)");
+    }
+}
