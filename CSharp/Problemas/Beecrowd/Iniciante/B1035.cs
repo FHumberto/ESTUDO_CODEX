11f9840b398a2355bbea4c41e8f210b044252722
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-26 05:57:14 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1035 e B1038

---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1035.cs
@@ -0,0 +1,23 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+internal class B1035 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Teste de Seleção 1");
+
+        string[] linha = Console.ReadLine().Split(' ');
+        int a = int.Parse(linha[0]);
+        int b = int.Parse(linha[1]);
+        int c = int.Parse(linha[2]);
+        int d = int.Parse(linha[3]);
+
+        string aceitos = (b > c && d > a && (c + d) > (a + b) && c > 0 && d > 0 && a % 2 == 0)
+            ? "Valores aceitos"
+            : "Valores nao aceitos";
+
+        Console.WriteLine(aceitos);
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/B1038.cs
@@ -0,0 +1,37 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante;
+internal class B1038 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Lanche");
+
+        string[] linha = Console.ReadLine().Split(' ');
+        int codigo = int.Parse(linha[0]);
+        int quantidade = int.Parse(linha[1]);
+        double total = 0;
+
+        switch (codigo)
+        {
+            case 1:
+                total = quantidade * 4;
+                break;
+            case 2:
+                total = quantidade * 4.5;
+                break;
+            case 3:
+                total = quantidade * 5;
+                break;
+            case 4:
+                total = quantidade * 2;
+                break;
+            case 5:
+                total = quantidade * 1.5;
+                break;
+        }
+
+        Console.WriteLine($"Total: R$ {total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
