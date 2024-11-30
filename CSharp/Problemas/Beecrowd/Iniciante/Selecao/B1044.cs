3ca577976d6fe4b5f29ddf2eacd56c90e1fcbd35
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-30 12:45:34 -0300

    [CSharp/Beecrowd] Add: resolução exercício B1044

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Selecao/B1044.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Selecao;
+
+internal class B1044 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Múltiplos");
+
+        string[] linha = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(linha[0]);
+        int b = int.Parse(linha[1]);
+
+        string mensagem = (a % b == 0 || b % a == 0) ? "Sao Multiplos" : "Nao sao Multiplos";
+
+        Console.WriteLine(mensagem);
+    }
+}
