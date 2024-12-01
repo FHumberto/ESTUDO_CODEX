ed2df8a4a21718874c5ca9fc1e0b265a1645e7ff
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-01 13:30:55 -0300

    [CSharp/Beecrowd] Add: resolução exercício B1050

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Selecao/B1050.cs
@@ -0,0 +1,35 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Selecao;
+
+internal class B1050 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("DDD");
+
+        Dictionary<int, string> dddCidades = new Dictionary<int, string>()
+        {
+            { 61, "Brasilia" },
+            { 71, "Salvador" },
+            { 11, "Sao Paulo" },
+            { 21, "Rio de Janeiro" },
+            { 32, "Juiz de Fora" },
+            { 19, "Campinas" },
+            { 27, "Vitoria" },
+            { 31, "Belo Horizonte" }
+        };
+
+        int codigo = int.Parse(Console.ReadLine());
+
+        if (dddCidades.TryGetValue(codigo, out string cidade))
+        {
+            Console.WriteLine(cidade);
+        }
+        else
+        {
+            Console.WriteLine("DDD nao cadastrado");
+        }
+    }
+}
