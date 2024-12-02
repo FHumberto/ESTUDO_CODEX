c792871e4a111a2dfb5c3aed3dc264047bf9fea6
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-02 06:37:56 -0300

    [CSharp/Beecrowd] Add: resolução exercício B1059

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1059.cs
@@ -0,0 +1,20 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1059 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Números Pares");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
