88e1228b3c2285fd624f865b6966443ac584be0d
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-16 05:45:54 -0300

    [CSharp/Beecrowd] Add: resolução exercício B1060

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1060.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1060 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Números Positivos");
+
+        float[] numeros = new float[6];
+        int positivos = 0;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = float.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            
+            if (numeros[i] > 0)
+            {
+                positivos++;
+            }
+        }
+
+        Console.WriteLine($"{positivos} valores positivos");
+    }
+}
