3a6f551ba7868df58d631e3e63b28a763050bff7
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-17 16:39:09 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1067

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1067.cs
@@ -0,0 +1,22 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1067 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Números Ímpares");
+
+        int x = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= x; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
