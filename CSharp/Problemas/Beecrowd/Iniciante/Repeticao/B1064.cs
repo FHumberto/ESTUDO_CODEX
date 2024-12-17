94ef4b4adf00840512a2b5c992f8757c5cfc40e2
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-17 07:05:58 -0300

    [CSharp/Beecrowd] Add: resolução exercício B1064

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1064.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1064 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Positivos e Média");
+
+        float[] valores = new float[6];
+        int positivos = 0;
+        float total = 0;
+        float media = 0;
+
+        for(int i = 0; i < valores.Length; i++)
+        {
+            valores[i] = float.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            if (valores[i] > 0)
+            {
+                positivos++;
+                total += valores[i];
+            }
+        }
+
+        media = total / positivos;
+
+        Console.WriteLine($"{positivos} valores positivos");
+        Console.WriteLine($"{media.ToString("F1", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
