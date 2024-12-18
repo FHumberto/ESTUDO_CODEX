e37cf439945af4cd4a7b3702199525ac14d924fc
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-18 08:39:29 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1070 e B1071

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1070.cs
@@ -0,0 +1,27 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1070 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Seis Números Ímpares");
+
+        int x = int.Parse(Console.ReadLine());
+        int numeroAtual = x;
+        int contadorImpares = 0;
+
+        while (contadorImpares < 6)
+        {
+            if (numeroAtual % 2 != 0)
+            {
+                Console.WriteLine(numeroAtual);
+                contadorImpares++;
+            }
+
+            numeroAtual++;
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1071.cs
@@ -0,0 +1,28 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1071 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Soma de Impares Consecutivos I");
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int somatorio = 0;
+        int inicial = Math.Min(a, b);
+        int final = Math.Max(a, b);
+
+        for (int i = inicial + 1; i < final; i++)
+        {
+            if (i % 2 != 0)
+            {
+                somatorio += i;
+            }
+        }
+
+        Console.WriteLine(somatorio);
+    }
+}
