1982f835ad61eb176e80f3fe99e878ef4dbeb56c
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-19 09:03:05 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1065

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Selecao/B1065.cs
@@ -0,0 +1,25 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Selecao;
+
+internal class B1065 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Pares entre Cinco Números");
+
+        int pares = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            int valores = int.Parse(Console.ReadLine());
+            if (valores % 2 == 0)
+            {
+                pares++;
+            }
+        }
+
+        Console.WriteLine($"{pares} valores pares");
+    }
+}
