585d6e2824d7d787e671728942a53d74c5f671a6
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-23 06:29:47 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1073 e B1075

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1075.cs
@@ -0,0 +1,22 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1075 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Resto 2");
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < 10000; i++)
+        {
+            if (i % n == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
