76d07a8ff231b717cf7b29cc8dc399021746af46
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-25 06:03:13 -0300

    [CSharp/Beecrowd] Add: resolução dos exercicios B1072 e B1078

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1072.cs
@@ -0,0 +1,33 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1072 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Intervalo 2");
+
+        int valores = int.Parse(Console.ReadLine());
+        int quantidadeIn = 0;
+        int quantidadeOut = 0;
+
+        for (int i = 0; i < valores; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 10 && numero <= 20)
+            {
+                quantidadeIn++;
+            }
+            else
+            {
+                quantidadeOut++;
+            }
+        }
+
+        Console.WriteLine($"{quantidadeIn} in");
+        Console.WriteLine($"{quantidadeOut} out");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1078.cs
@@ -0,0 +1,19 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1078 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Tabuada");
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{i} x {n} = {i * n}");
+        }
+    }
+}
