3044677ee77b1e9a8bb4cae632f4030c6f20be5f
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-27 03:49:50 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1066

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1066.cs
@@ -0,0 +1,39 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1066 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Pares, Ímpares, Positivos e Negativos");
+
+        const int Par = 0;
+        const int Impar = 1;
+        const int Positivo = 2;
+        const int Negativo = 3;
+
+        int[] valores = new int[4];
+
+        for (int i = 0; i < 5; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            valores[numero % 2 == 0 ? Par : Impar]++;
+            if (numero > 0)
+            {
+                valores[Positivo]++;
+            }
+            else if (numero < 0)
+            {
+                valores[Negativo]++;
+            }
+        }
+
+        Console.WriteLine($"{valores[Par]} valor(es) par(es)");
+        Console.WriteLine($"{valores[Impar]} valor(es) impar(es)");
+        Console.WriteLine($"{valores[Positivo]} valor(es) positivo(s)");
+        Console.WriteLine($"{valores[Negativo]} valor(es) negativo(s)");
+    }
+}
