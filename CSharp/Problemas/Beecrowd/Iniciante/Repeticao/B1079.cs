d8f0b73c3b055e101ca6e1a063ac2928879d00c9
Humberto Guedes <franciscohguedes@hotmail.com>
2024-12-30 02:25:33 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1079 e B1144

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1079.cs
@@ -0,0 +1,27 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1079 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Médias Ponderadas");
+
+        int casos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < casos; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(linha[0], System.Globalization.CultureInfo.InvariantCulture);
+            double b = double.Parse(linha[1], System.Globalization.CultureInfo.InvariantCulture);
+            double c = double.Parse(linha[2], System.Globalization.CultureInfo.InvariantCulture);
+
+            double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+            Console.WriteLine(media.ToString("F1", System.Globalization.CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1144.cs
@@ -0,0 +1,20 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1144 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequência Lógica");
+
+        int valor = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= valor; i++)
+        {
+            Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            Console.WriteLine($"{i} {Math.Pow(i, 2) + 1} {Math.Pow(i, 3) + 1}");
+        }
+    }
+}
