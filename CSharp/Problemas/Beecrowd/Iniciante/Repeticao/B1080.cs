e90cdb2917981769bee64fe125181accf8b38ee0
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-01 11:36:08 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1080 e B1099

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1080.cs
@@ -0,0 +1,30 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1080 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Maior e Posição");
+
+        int[] numeros = new int[100];
+        int maior = 0;
+        int posicao = 0;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+
+            if (numeros[i] > maior)
+            {
+                maior = numeros[i];
+                posicao = i + 1;
+            }
+        }
+
+        Console.WriteLine(maior);
+        Console.WriteLine(posicao);
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1099.cs
@@ -0,0 +1,37 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1099 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Soma de Ímpares Consecutivos II");
+
+        int casos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < casos; i++)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            int somatorio = 0;
+
+            int menor = Math.Min(x, y);
+            int maior = Math.Max(x, y);
+
+            for (int j = menor + 1; j < maior; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    somatorio += j;
+                }
+            }
+
+            Console.WriteLine(somatorio);
+        }
+    }
+}
