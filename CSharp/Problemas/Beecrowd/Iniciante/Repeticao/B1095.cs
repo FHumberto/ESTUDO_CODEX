0ad9c627cac9f11aefa114c05cfa3c287ab2f583
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-02 08:36:53 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1095 e B1101

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1095.cs
@@ -0,0 +1,25 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1095 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequencia IJ 1");
+
+        int i = 1;
+        int j = 60;
+
+        while (true)
+        {
+            if (j < 0)
+                break;
+
+            Console.WriteLine($"I={i} J={j}");
+            i += 3;
+            j -= 5;
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1101.cs
@@ -0,0 +1,36 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1101 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequência de Números e Soma");
+
+        while (true)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(linha[0]);
+            int y = int.Parse(linha[1]);
+
+            if (x <= 0 || y <= 0)
+                break;
+
+            int menor = Math.Min(x, y);
+            int maior = Math.Max(x, y);
+            int somatorio = 0;
+
+            for (int i = menor; i <= maior; i++)
+            {
+                Console.Write($"{i} ");
+                somatorio += i;
+            }
+
+            Console.WriteLine($"Sum={somatorio}");
+        };
+
+    }
+}
