f3935090cf3ad10f91ce7362f758c120e531ee24
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-03 08:49:38 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1113 e B1114

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1113.cs
@@ -0,0 +1,26 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1113 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Crescente e Decrecente");
+
+        while (true)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            int x = int.Parse(linha[0]);
+            int y = int.Parse(linha[1]);
+
+            if (x == y)
+                break;
+
+            string resposta = (y > x) ? "Crescente" : "Decrescente";
+
+            Console.WriteLine(resposta);
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1114.cs
@@ -0,0 +1,29 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1114 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Senha Fixa");
+
+        int senhaCorreta = 2002;
+
+        while (true)
+        {
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor == senhaCorreta)
+            {
+                Console.WriteLine("Acesso Permitido");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Senha Invalida");
+            }
+        }
+    }
+}
