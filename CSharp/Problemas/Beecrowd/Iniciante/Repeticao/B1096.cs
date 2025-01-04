23acdbec760754d4f1cf265c805b0e955570d1e9
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-04 09:40:51 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1096 e B1154

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1096.cs
@@ -0,0 +1,19 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1096 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequencia IJ 2");
+
+        for (int i = 1; i < 10; i += 2)
+        {
+            Console.WriteLine($"I={i} J=7");
+            Console.WriteLine($"I={i} J=6");
+            Console.WriteLine($"I={i} J=5");
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1154.cs
@@ -0,0 +1,29 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1154 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Idades");
+
+        int quantidadeDeCasos = 0;
+        int idades = 0;
+
+        while (true)
+        {
+            int dado = int.Parse(Console.ReadLine());
+
+            if (dado < 0)
+            {
+                Console.WriteLine($"{((float)idades / quantidadeDeCasos).ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+                break;
+            }
+
+            idades += dado;
+            quantidadeDeCasos++;
+        }
+    }
+}
