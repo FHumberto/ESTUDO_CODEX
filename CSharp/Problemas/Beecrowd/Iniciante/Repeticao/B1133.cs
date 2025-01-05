a061134ae1e3d62c73c9e1477362422e41fcf2eb
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-05 12:16:13 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1133 e B1134

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1133.cs
@@ -0,0 +1,26 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1133 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Resto da Divisão");
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int min = Math.Min(a, b);
+        int max = Math.Max(a, b);
+
+        for (int i = min + 1; i < max; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1134.cs
@@ -0,0 +1,40 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1134 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Tipo de Combustível");
+
+        bool iterator = false;
+        int[] contagem = new int[3];
+
+        while (!iterator)
+        {
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    contagem[0]++;
+                    break;
+                case 2:
+                    contagem[1]++;
+                    break;
+                case 3:
+                    contagem[2]++;
+                    break;
+                case 4:
+                    iterator = true;
+                    Console.WriteLine("MUITO OBRIGADO");
+                    Console.WriteLine($"Alcool: {contagem[0]}");
+                    Console.WriteLine($"Gasolina: {contagem[1]}");
+                    Console.WriteLine($"Diesel: {contagem[2]}");
+                    break;
+            }
+        }
+    }
+}
