8e56b0d7d7fd33c3a9917497962ef59435293eb2
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-07 08:37:30 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1142 e B1159

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1142.cs
@@ -0,0 +1,19 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1142 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("PUM");
+
+        int quantidade = int.Parse(Console.ReadLine());
+
+        for (int linha = 0, i = 1; linha < quantidade; linha++, i += 4)
+        {
+            Console.WriteLine($"{i} {i + 1} {i + 2} PUM");
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1159.cs
@@ -0,0 +1,33 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1159 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Soma de Pares Consecutivos");
+
+        int numero;
+
+        while ((numero = int.Parse(Console.ReadLine())) != 0)
+        {
+            int quantidadeDePares = 0;
+            int somatorio = 0;
+
+            while (quantidadeDePares < 5)
+            {
+                if (numero % 2 == 0)
+                {
+                    somatorio += numero;
+                    quantidadeDePares++;
+                }
+
+                numero++;
+            }
+
+            Console.WriteLine(somatorio);
+        }
+    }
+}
