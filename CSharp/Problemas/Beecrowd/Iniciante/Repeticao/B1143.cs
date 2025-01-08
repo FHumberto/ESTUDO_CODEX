a32915b5f9b4543af600e0dc30df4191fc102bc3
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-08 09:16:39 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1143

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1143.cs
@@ -0,0 +1,19 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1143 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Quadrado e ao Cubo");
+
+        int valor = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= valor; i++)
+        {
+            Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+        }
+    }
+}
