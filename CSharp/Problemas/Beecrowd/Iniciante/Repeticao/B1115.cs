266970e3cecfc5d19900a84ec392853259ad9143
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-11 17:26:41 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B115 e B1165

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1115.cs
@@ -0,0 +1,31 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1115 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Quadrante");
+
+        while (true)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            if (x == 0 || y == 0)
+                break;
+
+            if (x > 0 && y > 0)
+                Console.WriteLine("primeiro");
+            if (x < 0 && y > 0)
+                Console.WriteLine("segundo");
+            if (x < 0 && y < 0)
+                Console.WriteLine("terceiro");
+            if (x > 0 && y < 0)
+                Console.WriteLine("quarto");
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1165.cs
@@ -0,0 +1,47 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1165 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Número Primo");
+
+        int casos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < casos; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            string texto = EhPrimo(numero) ? "eh primo" : "nao eh primo";
+
+            Console.WriteLine($"{numero} {texto}");
+        }
+    }
+
+    static bool EhPrimo(int numero)
+    {
+        // 1 não é primo
+        if (numero <= 1)
+            return false;
+
+        // 2 e 3 não são primos
+        if (numero == 2 || numero == 3)
+            return true;
+
+        //! divisibilidade de 2 e 3, for 0 não é primo
+        if (numero % 2 == 0 || numero % 3 == 0)
+            return false;
+
+        //! se tem divisores ímpares de 5 até a potencia
+        for (int i = 5; i * i <= numero; i += 6)
+        {
+            if (numero % i == 0 || numero % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
