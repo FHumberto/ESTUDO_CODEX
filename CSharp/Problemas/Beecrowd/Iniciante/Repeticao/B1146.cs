7cdbcd1bb526fe7074ceca322309a712abf133c9
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-12 15:21:28 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1146 e B1153

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1146.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1146 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequências Crescentes");
+
+        while (true)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero == 0)
+                break;
+
+            //! console.write é menos eficiente que o string builder
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado.Append(i);
+
+                if (i < numero)
+                    resultado.Append(' ');
+            }
+
+            Console.WriteLine(resultado.ToString());
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1153.cs
@@ -0,0 +1,31 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1153 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Fatorial Simples");
+
+        int numero = int.Parse(Console.ReadLine());
+        int resultado = 0;
+
+        if (numero == 0 || numero == 1)
+        {
+            resultado = 1;
+        }
+        else
+        {
+            resultado = 1;
+
+            for (int i = numero; i >= 1; i--)
+            {
+                resultado *= i;
+            }
+        }
+
+        Console.WriteLine(resultado);
+    }
+}
