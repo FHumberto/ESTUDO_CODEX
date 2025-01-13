f34154903df3ebc8cd05b44f166f81422f898ba1
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-13 09:49:55 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1132

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1132.cs
@@ -0,0 +1,29 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1132 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Múltiplos de 13");
+
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int min = Math.Min(x, y);
+        int max = Math.Max(x, y);
+        int somatorio = 0;
+
+        for (int i = min; i <= max; i++)
+        {
+            if (i % 13 != 0)
+            {
+                somatorio += i;
+            }
+        }
+
+        Console.WriteLine(somatorio);
+    }
+}
