3bd21bc2ce4197731838b2710a23dc03050d9ca9
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-14 09:16:23 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1172 e B1173

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Vetores/B1172.cs
@@ -0,0 +1,23 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Vetores;
+
+internal class B1172 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Substituição em Vetor I");
+
+        int[] valores = new int[10];
+
+        for (int i = 0; i < valores.Length; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            valores[i] = (numero <= 0) ? 1 : numero;
+
+            Console.WriteLine($"X[{i}] = {valores[i]}");
+        }
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Vetores/B1173.cs
@@ -0,0 +1,20 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Vetores;
+
+internal class B1173 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Preenchimento de Vetor I");
+
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"N[{i}] = {numero}");
+            numero *= 2;
+        }
+    }
+}
