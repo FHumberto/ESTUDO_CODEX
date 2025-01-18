6354dc7b1ef88f2065adffdf8a73edcde0cd0760
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-18 17:08:57 -0300

    [Beecrowd/CSharp] Add: resolução do exercício B1175

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Vetores/B1175.cs
@@ -0,0 +1,26 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Vetores;
+
+internal class B1175 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Troca em Vetor I");
+
+        int[] vetor = new int[20];
+
+        for (int i = 0; i < 20; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Reverse(vetor);
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine($"N[{i}] = {vetor[i]}");
+        }
+    }
+}
