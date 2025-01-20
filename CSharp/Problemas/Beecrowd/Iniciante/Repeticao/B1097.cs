3f8ff059e9a2e8ec57b848688527b1804eff7db6
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-20 06:21:47 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1097

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1097.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1097 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Sequencia IJ3");
+
+        int j = 7;
+
+        for (int i = 1; i <= 9; i += 2, j += 2)
+        {
+            Console.WriteLine($"I={i} J={j}");
+            Console.WriteLine($"I={i} J={j - 1}");
+            Console.WriteLine($"I={i} J={j - 2}");
+        }
+    }
+}
