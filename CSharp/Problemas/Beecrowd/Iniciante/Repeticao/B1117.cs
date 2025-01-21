3cb2719ca5114fbef8228d76e27d6bf43a298925
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-21 04:38:02 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1117

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1117.cs
@@ -0,0 +1,32 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1117 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Validação de Nota");
+
+        float notas = 0f;
+        int aux = 0;
+
+        while (aux < 2)
+        {
+            float nota = float.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            if (nota >= 0 && nota <= 10)
+            {
+                notas += nota;
+                aux++;
+            }
+            else
+            {
+                Console.WriteLine("nota invalida");
+            }
+        }
+
+        Console.WriteLine($"media = {(notas / 2).ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+    }
+}
