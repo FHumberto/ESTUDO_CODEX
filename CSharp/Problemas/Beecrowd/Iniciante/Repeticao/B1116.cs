f565b1afb3363512296e2e7f4d25270ad7f3b723
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-22 05:44:03 -0300

    [CSharp/Beecrowd] Add: resolução do exercício B1116

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1116.cs
@@ -0,0 +1,32 @@
+﻿using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1116 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Dividindo X por Y");
+
+        int casos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < casos; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(linha[0]);
+            int b = int.Parse(linha[1]);
+
+            if (b == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double divisao = (double)a / b;
+                Console.WriteLine($"{divisao.ToString("F1", System.Globalization.CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
