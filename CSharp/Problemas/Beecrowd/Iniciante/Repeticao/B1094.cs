bc250accdf2217c09dc27ddbe7d5aea6aa12192d
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-26 14:33:08 -0300

    [CSharp/Beecrowd] Add: resolução dos exercícios B1094 e B1118

---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1094.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+using Beecrowd.Template;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1094 : Problema
+{
+    public override void PMain()
+    {
+        const int Rato = 0;
+        const int Sapo = 1;
+        const int Coelho = 2;
+
+        int casos = int.Parse(Console.ReadLine());
+        int[] experimentos = new int[3];
+
+        for (int i = 0; i < casos; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            int qtd = int.Parse(linha[0]);
+            char cobaia = char.Parse(linha[1]);
+
+            switch (cobaia)
+            {
+                case 'R':
+                    experimentos[Rato] += qtd;
+                    break;
+
+                case 'S':
+                    experimentos[Sapo] += qtd;
+                    break;
+
+                case 'C':
+                    experimentos[Coelho] += qtd;
+                    break;
+            }
+        }
+
+        int totalExperimentos = experimentos[Coelho] + experimentos[Rato] + experimentos[Sapo];
+
+        Console.WriteLine($"Total: {totalExperimentos} cobaias");
+        Console.WriteLine($"Total de coelhos: {experimentos[Coelho]}");
+        Console.WriteLine($"Total de ratos: {experimentos[Rato]}");
+        Console.WriteLine($"Total de sapos: {experimentos[Sapo]}");
+        Console.WriteLine($"Percentual de coelhos: {((experimentos[Coelho] / (float)totalExperimentos) * 100).ToString("F2", CultureInfo.InvariantCulture)} %");
+        Console.WriteLine($"Percentual de ratos: {((experimentos[Rato] / (float)totalExperimentos) * 100).ToString("F2", CultureInfo.InvariantCulture)} %");
+        Console.WriteLine($"Percentual de sapos: {((experimentos[Sapo] / (float)totalExperimentos) * 100).ToString("F2", CultureInfo.InvariantCulture)} %");
+    }
+}
---
+++ CSharp/Problemas/Beecrowd/Iniciante/Repeticao/B1118.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+using Beecrowd.Template;
+using Beecrowd.Util;
+
+namespace Beecrowd.Iniciante.Repeticao;
+
+internal class B1118 : Problema
+{
+    public override void PMain()
+    {
+        ProcedureHelpers.Cabecalho("Várias Notas com Validação");
+
+        int codigo = 0;
+
+        do
+        {
+            int qtd = 0;
+            float notas = 0;
+
+            while (qtd != 2)
+            {
+                float nota = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (nota < 0 || nota > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                else
+                {
+                    notas += nota;
+                    qtd++;
+                }
+            }
+
+            float media = notas / 2;
+
+            Console.WriteLine($"media = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            while (true)
+            {
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                codigo = int.Parse(Console.ReadLine());
+
+                if (codigo == 1 || codigo == 2)
+                {
+                    break;
+                }
+            }
+
+        } while (codigo != 2);
+    }
+}
